499e6b5845ab30442921047ce3743a6633634bd8
mariata06 <slinkywizard@mail.ru>
2020-03-02 16:57:06 +0300

    Внесены изменения в CSS, script
    Изменила сценарий появления формы, изменила анимацию

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 var button = document.querySelector(".openform-button");
+var popup = document.querySelector(".search-form");   /*внешняя обертка формы*/
+var form = popup.querySelector("form");  /*в обертке находим саму <форму>*/
 
-var popup = document.querySelector(".search-form");
+var adultsNumber = form.querySelector("[value=adultsNumber]");
 
-/*var close = popup.querySelector(".modal-close");       кнопка закрытия формы Крестик- В Седоне этого нет*/
+var childrenNumber = form.querySelector("[value=childrenNumber]");
 
-var form = popup.querySelector(".booking-form"); /*form-wrapper*/
-
-var adultsNumber = popup.querySelector("[value=adultsNumber]");
-
-var childrenNumber = popup.querySelector("[value=childrenNumber]");
-
-/*var userMessage = popup.querySelector("[name=userMessage]");*/
+var checkin = form.querySelector(".checkin"); 
 
 var isStorageSupport = true;
 var currentAdultsNumber = "";
@@ -23,33 +19,28 @@ try {
   isStorageSupport = false;
 }
 
+popup.classList.add("search-form-hide");
+
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
+  popup.classList.toggle("search-form-hide");
   popup.classList.add("search-form-show");
-  adultsNumber.focus();
+  popup.classList.remove("search-form-error");
   if (currentAdultsNumber) {
     adultsNumber.value = currentAdultsNumber;
     childrenNumber.focus();
   } else {
     adultsNumber.focus();
   }
-});
-  /*
-  if (curChildNumber) {
-    userEmail.value = currentEmail;
-    userMessage.focus();
+  
+  if (currentChildrenNumber) {
+    childrenNumber.value = currentChildrenNumber;
+    checkin.focus();
   } else {
-    userEmail.focus();
+    childrenNumber.focus();
   }
-});*/
-
-/*
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("search-form-show");
-  popup.classList.remove("search-form-error");
 });
-*/
+  
 form.addEventListener("submit", function (evt) {
   if (!adultsNumber.value || !childrenNumber.value) {
     evt.preventDefault();
